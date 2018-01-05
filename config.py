073d93887d5ff342c9e1187763953994c473bd60
cryptonew <cryptonew2017@gmail.com>
2018-01-05 22:18:34 +0200

    firts commit

---
+++ config.py
@@ -0,0 +1,5 @@
+#config
+zecAddress = "t1U3FRoPEAVzUdeMRkKL2MhhhLokPKYoFzJ" #NanoPool ZCASH address to monitor
+iftttKey = "bOFKmLQvh9S_T4Yvvwcx_c" #key from IFTTT Maker applet
+workers_num = 1 # count of your workers
+
